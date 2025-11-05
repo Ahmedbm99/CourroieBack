06617bf3019b8205b2fb0c53a2b1319076f5fd28
Ahmed Ben Makhlouf <101886914+Ahmedbm99@users.noreply.github.com>
2025-11-06 00:44:04 +0100

    fix

--- src/server.js
+++ src/server.js
@@ -16,12 +16,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
-});
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type'],
+}));
 require("./routes")(app);
 app.use('/public', express.static(path.join(__dirname, '../public'), {
     setHeaders: (res, path) => {
