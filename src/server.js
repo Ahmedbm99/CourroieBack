b7aa7c16565019479a4fdea05dc0d1645c4fe1ab
Ahmed Ben Makhlouf <101886914+Ahmedbm99@users.noreply.github.com>
2025-11-11 18:57:05 +0100

    adding courroie_image&courroie_type&courroie_fiche

--- src/server.js
+++ src/server.js
@@ -16,7 +16,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
+    res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
