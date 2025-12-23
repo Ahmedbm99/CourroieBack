d725d581579bcd1dc4ae4c32413b251ef4863f42
Ahmed Ben Makhlouf <101886914+Ahmedbm99@users.noreply.github.com>
2025-12-23 20:44:57 +0100

    cors

--- src/server.js
+++ src/server.js
@@ -16,7 +16,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
+    res.setHeader('Access-Control-Allow-Origin', 'https://ahmedbm99.github.io');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
